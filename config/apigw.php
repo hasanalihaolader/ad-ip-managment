4649c1967c46578dfd3afdd167a0bd556dd05c2f
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-09-22 02:48:44 +0600

    feature: share to check apigw is request to through api using apigw configuration

---
+++ config/apigw.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | APIGW MS Configurations
+    |--------------------------------------------------------------------------
+    */
+    'service_id' => env('APIGW_SERVICE_ID'),
+    'service_key' => env('APIGW_SERVICE_KEY'),
+    'service_name' => env('APIGW_SERVICE_NAME'),
+];
